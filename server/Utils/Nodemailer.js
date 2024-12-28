bf768efade516c16bce4058d487531c2bfe34591
mannumishra <mannumishra2207@outlook.com>
2024-12-28 13:46:32 +0530

    add chnages

--- server/Utils/Nodemailer.js
+++ server/Utils/Nodemailer.js
@@ -4,7 +4,7 @@ const transporte = nodemailer.createTransport({
     service:process.env.MAIL_SERVICE,
     auth:{
         user:process.env.SEND_MAIL_ID,
-        pass:process.env.MAIL_PASSWORD
+        pass:process.env.PASS
     }
 })
 
